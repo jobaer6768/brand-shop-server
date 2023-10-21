0debbff5d2b542e28073fb1f67ad90e14ee14298
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-10-21 16:55:47 +0600

    added brands in database

--- index.js
+++ index.js
@@ -28,12 +28,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const brandCollection = client.db("brandDB").collection("brands");
+
+        // brands
+        app.post('/brands', async (req, res) => {
+            const brand = req.body;
+            console.log(brand);
+
+            const result = await brandCollection.insertOne(brand);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
