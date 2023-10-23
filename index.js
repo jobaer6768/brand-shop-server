7856ef8f49e70c535c5ce62337e2177df2e0b9d3
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-10-23 16:34:27 +0600

    applied get method for a single product by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -45,6 +45,16 @@ async function run() {
             const query = { brand_name: brand_name };
             const cursor = productCollection.find(query);
             const result = await cursor.toArray();
+
+            res.send(result);
+        })
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+
+            const filter = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(filter);
 
             res.send(result);
         })
